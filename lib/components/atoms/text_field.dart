6d0837e636a9f5d639c66b8d16c0634d2baf5408
DELBAR HUSSAIN <147152914+Delbar2004@users.noreply.github.com>
2026-03-02 17:16:15 +0530

    Updated text_field atom component

--- lib/components/atoms/text_field.dart
+++ lib/components/atoms/text_field.dart
@@ -1,10 +1,10 @@
+import 'dart:ui';
 import 'package:flutter/material.dart' as m;
 import 'package:flutter/services.dart';
 
 class TextField extends m.StatefulWidget {
   final String hintText;
   final String? Function(String)? validator;
-  final int? maxLength;
   final List<TextInputFormatter>? inputFormatters;
   final double? width;
 
@@ -18,7 +18,6 @@ class TextField extends m.StatefulWidget {
     super.key,
     required this.hintText,
     this.validator,
-    this.maxLength,
     this.inputFormatters,
     this.width,
     this.fontSize = 22,
@@ -55,47 +54,55 @@ class _TextFieldState extends m.State<TextField> {
       width: widget.width,
       child: m.Column(
         mainAxisSize: m.MainAxisSize.min,
-        crossAxisAlignment: m.CrossAxisAlignment.start,
+        crossAxisAlignment: m.CrossAxisAlignment.center, // Center the column content
         children: [
           m.MouseRegion(
             onEnter: (_) => setState(() => _isHovering = true),
             onExit: (_) => setState(() => _isHovering = false),
-            child: m.AnimatedContainer(
-              duration: const Duration(milliseconds: 150),
-              decoration: m.BoxDecoration(
-                color: _isHovering
-                    ? widget.fillColor.withOpacity(0.05)
-                    : widget.fillColor,
-                borderRadius: m.BorderRadius.circular(widget.borderRadius), // Pill shape
-                border: m.Border.all(
-                  color: hasError ? m.Colors.red : m.Colors.white.withOpacity(0.8),
-                  width: 1.5,
-                ),
-              ),
-              child: m.TextField(
-                controller: _controller,
-                obscureText: false,
-                maxLength: widget.maxLength,
-                inputFormatters: widget.inputFormatters,
-                onChanged: _validate,
-                style: m.TextStyle(
-                  fontSize: widget.fontSize,
-                  color: widget.textColor,
-                  fontWeight: widget.fontWeight,
-                ),
-                textAlignVertical: m.TextAlignVertical.center,
-                decoration: m.InputDecoration(
-                  counterText: "",
-                  hintText: widget.hintText,
-                  hintStyle: m.TextStyle(
-                    color: widget.textColor.withOpacity(0.5),
-                    fontSize: widget.fontSize - 2,
+            child: m.ClipRRect(
+              borderRadius: m.BorderRadius.circular(widget.borderRadius),
+              child: m.BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
+                child: m.AnimatedContainer(
+                  duration: const Duration(milliseconds: 150),
+                  decoration: m.BoxDecoration(
+                    color: _isHovering
+                        ? widget.fillColor.withOpacity(0.2)
+                        : widget.fillColor.withOpacity(0.1),
+                    borderRadius: m.BorderRadius.circular(widget.borderRadius),
+                    border: m.Border.all(
+                      color: hasError
+                          ? m.Colors.red.withOpacity(0.5)
+                          : widget.fillColor.withOpacity(0.3),
+                      width: 1.5,
+                    ),
                   ),
-                  contentPadding: const m.EdgeInsets.symmetric(
-                    horizontal: 24,
-                    vertical: 16,
+                  child: m.TextField(
+                    controller: _controller,
+                    obscureText: false,
+                    inputFormatters: widget.inputFormatters,
+                    onChanged: _validate,
+                    textAlign: m.TextAlign.center,
+                    style: m.TextStyle(
+                      fontSize: widget.fontSize,
+                      color: widget.textColor,
+                      fontWeight: widget.fontWeight,
+                    ),
+                    textAlignVertical: m.TextAlignVertical.center,
+                    decoration: m.InputDecoration(
+                      counterText: "",
+                      hintText: widget.hintText,
+                      hintStyle: m.TextStyle(
+                        color: widget.textColor.withOpacity(0.4),
+                        fontSize: widget.fontSize - 2,
+                      ),
+                      contentPadding: const m.EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 16,
+                      ),
+                      border: m.InputBorder.none,
+                    ),
                   ),
-                  border: m.InputBorder.none,
                 ),
               ),
             ),
@@ -103,12 +110,13 @@ class _TextFieldState extends m.State<TextField> {
           if (hasError) ...[
             const m.SizedBox(height: 8),
             m.Padding(
-              padding: const m.EdgeInsets.only(left: 20),
+              padding: const m.EdgeInsets.symmetric(horizontal: 20),
               child: m.Text(
                 _errorText!,
+                textAlign: m.TextAlign.center,
                 style: const m.TextStyle(
-                  color: m.Colors.red,
-                  fontSize: 17,
+                  color: m.Colors.redAccent,
+                  fontSize: 14,
                   fontWeight: m.FontWeight.w500,
                 ),
               ),
